837208543d488fbafddfde093dfaedec32785c46
Darsh Ranjan <dranjan@berkeley.edu>
2025-01-17 19:26:01 -0800

    Remove recursion

--- run.py
+++ run.py
@@ -6,17 +6,18 @@ cmap = colormaps["plasma"]
 
 
 def generate(bits):
-    if not bits:
-        return np.array([[0]])
-    A = generate(bits - 1)
-    n = 2**(bits - 1)
-    N = n*n
-    result = np.empty((2*n, 2*n), dtype=int)
-    result[:n, :n] = A.T
-    result[n:, :n] = A + N
-    result[n:, n:] = A + 2*N
-    result[:n, n:] = A.T[::-1, ::-1] + 3*N
-    return result
+    A = np.array([[0]])
+    n = 1
+    for _ in range(bits):
+        N = n*n
+        B = np.empty((2*n, 2*n), dtype=int)
+        B[:n, :n] = A.T
+        B[n:, :n] = A + N
+        B[n:, n:] = A + 2*N
+        B[:n, n:] = A.T[::-1, ::-1] + 3*N
+        n += n
+        A = B
+    return A
 
 
 bits = 8
