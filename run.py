746e6d6dee4adfe22058e595e62fede6601614fd
Darsh Ranjan <dranjan@berkeley.edu>
2025-01-21 21:36:27 -0800

    Output a padded image as well

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 """
 Usage: python run.py
 
-Generates a visualization of a Hilbert curve. The output is saved in
-build/output.png.
+Generates a visualization of a Hilbert curve.
+
+There are two outputs:
+1. ./build/favicon.ico
+2. ./build/output-padded.png
 """
 import os
 
@@ -11,7 +14,11 @@ import numpy as np
 import PIL
 
 
-def hilbert_curve_coordinate(bits):
+def generate_image(bits):
+    """
+    Generate the single-channel image. Its shape will be 2**bits by 2**bits,
+    and its values will be in the interval [0, 1).
+    """
     A = np.array([[0]])
     n = 1
     for _ in range(bits):
@@ -23,8 +30,22 @@ def hilbert_curve_coordinate(bits):
     return A / (n*n)
 
 
+def pad_image(image, vpad=0.5):
+    """
+    Add a transparent border to the given image. The result will be (1 + vpad)
+    times as large as the input on the vertical axis, with equal padding on all
+    four sides.
+    """
+    size0, size1 = image.shape[:2]
+    offset = int(size0 * (vpad/2))
+    image2 = np.zeros((size0 + 2*offset, size1 + 2*offset, 4), dtype=np.uint8)
+    image2[offset:offset + size0, offset:offset + size1] = image
+    return image2
+
+
 bits = 8
-A = hilbert_curve_coordinate(bits)
-image = PIL.Image.fromarray(colormaps["plasma"](A[::-1], bytes=True))
+A = generate_image(bits)
+image = colormaps["plasma"](A[::-1], bytes=True)
 os.makedirs('build', exist_ok=True)
-image.save('build/favicon.ico')
+PIL.Image.fromarray(image).save('build/favicon.ico')
+PIL.Image.fromarray(pad_image(image)).save('build/output-padded.png')
