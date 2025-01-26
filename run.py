f1da99c8f373774a00d3bea00877be17f620c467
Darsh Ranjan <dranjan@berkeley.edu>
2025-01-25 20:59:13 -0800

    Refactor

--- run.py
+++ run.py
@@ -4,6 +4,7 @@
 #
 # /// script
 # dependencies = [
+#     "cmasher",
 #     "matplotlib",
 #     "numpy",
 #     "Pillow",
@@ -28,6 +29,8 @@ import PIL
 import scipy.signal
 import scipy.ndimage
 
+import cmasher as cmr
+
 
 def hilbert_data(bits):
     """
@@ -49,17 +52,46 @@ def cyclic_hilbert_data(bits):
     return np.block([[A[::-1, ::-1], A[::-1, ::-1] + 3], [A + 1, A + 2]])/4
 
 
-def pad_image(image, vpad=1.0):
+def color_image(x, cyclic=False,  v1=25, v2=0.6, v3=2, v4=0.93, v5=1.5):
     """
-    Add a transparent border to the given image. The result will be (1 + vpad)
-    times as large as the input on the vertical axis, with equal padding on all
-    four sides.
+    Get RGBA data from the Hilbert curve coordinates. Edges will be
+    darkened, and corners will be rounded.
+
+    TODO: the parameters above are pretty brittle and difficult to tweak
+    manually. The defaults don't work well for all image sizes, and the
+    parameters' behaviors are pretty tightly coupled.
     """
-    size0, size1 = image.shape[:2]
-    offset = int(size0 * (vpad/2))
-    image2 = np.zeros((size0 + 2*offset, size1 + 2*offset, 4), dtype=np.uint8)
-    image2[offset:offset + size0, offset:offset + size1] = image
-    return image2
+    # First we compute a mask to darken edges.
+    e = scipy.signal.convolve(x + 4, edge_filter(5), mode='same')
+    e = np.abs(e)
+    if cyclic:
+        # This is a quick hack that works well in practice, not
+        # something mathematically principled.
+        e = np.minimum(e, e[::-1])
+
+    mask = 1/(1 + ((e - e.min())*v1)**v2)**v3
+
+    # Now we do some morphology here to enhance corners.
+    masks = [mask]
+    for n in [1, 2, 3, 4]:
+        masks.append(maxmin_filter(mask, circular_stencil(n)))
+    mask = np.sum(masks, axis=0)/len(masks)
+
+    # The mask logic above darkens the whole image by a little bit. This
+    # correction brings the lightness back up.
+    mask = mask / v4
+    mask0 = mask <= 1
+    mask1 = mask0 ^ True
+
+    # It's worth experimenting with different colormaps here.
+    # We want something that's perceptually uniform and doesn't get too
+    # light or dark.
+    cmap = plt.get_cmap('cmr.seasons' if cyclic else 'plasma')
+    y = cmap(x)
+    y[mask0, :3] *= mask[mask0, None]
+    y[mask1, :3] = 1 - (1 - y[mask1, :3])*(v5 - mask[mask1, None])/(v5 - 1)
+
+    return y
 
 
 def edge_filter(n):
@@ -94,49 +126,41 @@ def maxmin_filter(x, stencil):
     return z
 
 
-# This is the "pure" 2D Hilbert curve data. It makes a fine image on its
-# own, but we'll do some further processing to enhance edges and
-# corners.
+def pad_image(image, vpad=1.0, r=10, s=(3, 3)):
+    """
+    Add a transparent border to the given image. The result will be (1 + vpad)
+    times as large as the input on the vertical axis, with equal padding on all
+    four sides.
+
+    The image will have a black border that fades away from the image. A shift
+    can also be applied to add the illusion of depth.
+    """
+    size0, size1 = image.shape[:2]
+    offset = int(size0 * (vpad/2))
+    image2 = np.zeros((size0 + 2*offset, size1 + 2*offset, 4), dtype=np.uint8)
+    image2[offset:offset + size0, offset:offset + size1] = image
+
+    # Here's the black border and shadow. distance_transforme_edt is a
+    # little bit too much machinery for what we're doing here, but we
+    # already have the dependency and it's a one-liner, so...
+    bg = np.ones(image2.shape[:2])
+    bg[offset:offset + size0, offset:offset + size1] = 0
+    d = scipy.ndimage.distance_transform_edt(bg)
+    #alpha = np.maximum(0, r - d) / r
+    alpha = np.exp(-0.5*(d/r)**2)
+    alpha = (alpha*255).astype(np.uint8)
+    alpha = np.roll(alpha, s[0], axis=0)
+    alpha = np.roll(alpha, s[1], axis=1)
+
+    image2[..., 3] = np.maximum(image2[..., 3], alpha)
+
+    return image2
+
+
+cyclic = False
 bits = 8
-A = hilbert_data(bits)
-
-# We do some edge enhancement here, specifically making them darker.
-# First we apply a linear edge detection filter, with some nonlinear
-# postprocessing.
-e = scipy.signal.convolve(A + 4, edge_filter(5), mode='same')
-e = np.abs(e)
-v0 = e.min()
-
-# These magic numbers were tweaked until the result was acceptable.
-v1 = 25
-v2 = 0.6
-v3 = 4
-
-mask = 1/(1 + ((e - v0)*v1)**v2)**v3
-
-# Finally we do some morphology here to enhance corners.
-masks = [mask]
-for n in [1, 2, 3, 4]:
-    masks.append(maxmin_filter(mask, circular_stencil(n)))
-mask = np.sum(masks, axis=0)/len(masks)
-
-# More magic numbers. The mask logic above darkens the whole image by
-# a little bit. This correction brings the lightness back up.
-v4 = 0.88
-v5 = 1.5
-mask = mask / v4
-mask0 = mask <= 1
-mask1 = mask0 ^ True
-
-# It's worth experimenting with different colormaps here.
-# We want something that's perceptually uniform and doesn't get too
-# light or dark.
-cmap = plt.get_cmap('plasma')
-B = cmap(A)
-B[mask0, :3] *= mask[mask0, None]
-B[mask1, :3] = 1 - (1 - B[mask1, :3])*(v5 - mask[mask1, None])/(v5 - 1)
-#B[..., 3] = mask
-
+A = (cyclic_hilbert_data if cyclic else hilbert_data)(bits)
+B = color_image(A, cyclic=cyclic)
 image = (B[::-1]*255).astype(np.uint8)
 os.makedirs('build', exist_ok=True)
 PIL.Image.fromarray(image).save('build/favicon.ico')
